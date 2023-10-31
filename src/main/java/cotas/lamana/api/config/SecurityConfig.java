ef03d8233cdadf403660f7d3a5e6edd5a70835ac
talestrejuli <75254536+talestrejuli@users.noreply.github.com>
2023-10-31 14:18:40 -0300

    Retomando funcionamento criptografia

---
+++ src/main/java/cotas/lamana/api/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package cotas.lamana.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/usuarios/cadastrar")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/usuarios/confirmar-email")).permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+
+    }
+}
